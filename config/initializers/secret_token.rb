859890cabc1264eb6eb529997296529169cde261
GT <gambit_tides@hotmail.com>
2012-11-29 07:29:12 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CSflu::Application.config.secret_token = 'ace02407a54fa8b714aa17ada57b01109909f817c83ec0133bc096272e035238000f695336a7377143bb2bfefa340dc4e158963e72e800b6fda9c5b78551b198'
